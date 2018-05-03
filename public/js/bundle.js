3637c30c70c77a7dd39a9c8e1971c0593506d7e9
Mike Vanis <hello@mikevanis.com>
2018-05-03 01:23:48 +0100

    map working

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -55,7 +55,9 @@ function setupMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiaW50ZXJhY3Rpb25yZXNlYXJjaHN0dWRpbyIsImEiOiJjamdwcXd1bTMwMmNpMnhwZWU1NTRibWg4In0.Z0N-6EZWHB1cawLd1Hz_2A';
     var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-0.038564, 51.4735455],
+    zoom: 17
     });
 }
 
