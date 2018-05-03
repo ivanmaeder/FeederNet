52ff4d55252e83239d15c831b2a337ce3dc6b556
Mike Vanis <hello@mikevanis.com>
2018-05-03 14:40:47 +0100

    DB connection test

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mysql = require('mysql');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
+
+var mysqlConnection = mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
+});
+
+mysqlConnection.connect(function(err) {
+    if (err) {
+        console.log("ERROR: Database connection failed: " + err.stack);
+        return;
+    }
+    console.log("INFO: Connected to database.");
+});
+
+connection.end();
 
 app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
@@ -18,5 +36,9 @@ io.on('connection', function(socket) {
     socket.on('heartbeat', function(data) {
         console.log("INFO: Received heartbeat with data: ");
         console.log(data);
+    });
+
+    socket.on('newTrack', function(data) {
+        console.log("INFO: Received new track event!");
     });
 });
