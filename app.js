0e3a05247a41e1beb952e78dab9af3d3d56e1a60
Mike Vanis <hello@mikevanis.com>
2018-05-08 18:09:59 +0100

    added newTrack socket event

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var mysqlConnection = mysql.createConnection({
     port: process.env.RDS_PORT
 });
 
+// Connected feeder array
 var connectedFeeders = new Array();
 
 // Connect to MySQL database
@@ -25,30 +26,60 @@ mysqlConnection.connect(function(err) {
 
 mysqlConnection.end();
 
+function logTrack(name, timedate, rfid) {
+    // Connect to MySQL database
+    mysqlConnection.connect(function(err) {
+        if (err) {
+            console.log("ERROR: Database connection failed: " + err.stack);
+            return;
+        }
+        var sql = "INSERT INTO log (feedername, timedate, rfid) VALUES ?";
+        var values = [
+            [name, timedate, rfid]
+        ];
+        mysqlConnection.query(sql, [values], function (err, result) {
+            if (err) {
+                console.log("ERROR: SQL insertion failed.");
+                return;
+            }
+            console.log("INFO: Inserted: " + result.affectedRows);
+            mysqlConnection.end();
+        });
+    });
+
+}
+
+// Express setup
 app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
 
+// Server setup
 server.listen(app.get('port'), function() {
     console.log('Node version: ' + process.versions.node);
     console.log('Server listening on port ' + app.get('port'));
 });
 
+// Socket events
 io.on('connection', function(socket) {
     console.log("INFO: New socket connection opened.");
 
     // Emit an ID request to the current socket to see if it's a feeder.
     socket.emit('idRequest', " ");
 
+    // Insert feeder data to database.
     socket.on('newTrack', function(data) {
         console.log("INFO: Received new track event!");
+        logTrack(data.feederName, data.timedate, data.rfid);
     });
 
+    // Receive new feeder's ID.
     socket.on('idTransmit', function(data) {
         console.log("INFO: Received new feeder ID " + data.feederName);
         connectedFeeders.push({feederName: data.feederName, socketID: socket.id});
         console.log("INFO: Associated " + data.feederName + " with ID " + socket.id);
     });
 
+    // Detect disconnected feeder.
     socket.on('disconnect', function() {
         console.log("INFO: Socket disconnected. Socket ID: " + socket.id);
         for (var key in connectedFeeders) {
