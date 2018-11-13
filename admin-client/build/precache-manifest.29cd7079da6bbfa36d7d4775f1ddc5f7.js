4a5dae7557ca0b52442b0d27273da4e029392f8b
mikevanis <hello@mikevanis.com>
2018-11-13 13:08:32 +0000

    cleanup and add build dir

---
+++ admin-client/build/precache-manifest.29cd7079da6bbfa36d7d4775f1ddc5f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5489e06dfe97ea672da3",
+    "url": "/static/css/main.c133bddc.chunk.css"
+  },
+  {
+    "revision": "5489e06dfe97ea672da3",
+    "url": "/static/js/main.5489e06d.chunk.js"
+  },
+  {
+    "revision": "7e35937a85aa11e732d5",
+    "url": "/static/js/1.7e35937a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b3ce5876fa6389865bc9279849745e0",
+    "url": "/index.html"
+  }
+];
